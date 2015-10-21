4fa4a6aed748c36af6bbb4577bb542493eb32358
Philippe Massicotte <pmassicotte@hotmail.com>
2015-10-21 15:38:06 +0200

    using 700 data points to explore relation between axx and a350

--- fit_cdom.R
+++ fit_cdom.R
@@ -1,20 +1,21 @@
 rm(list = ls())
 
-spectra <- readRDS("data/clean/spectra.rds")
+spectra_massicotte <- readRDS("dataset/clean/spectra_massicotte.rds")
+spectra_asmala2014 <- readRDS("dataset/clean/spectra_asmala2014.rds")
 
-
+spectra <- bind_rows(spectra_massicotte, spectra_asmala2014)
 
 # quick plot of raw data --------------------------------------------------
-ggplot(spectra, aes(x = wavelength, y = absorbance, group = sample)) +
-  geom_line(size = 0.1) +
+ggplot(spectra, aes(x = wavelength, y = absorption, group = sample_id)) +
+  geom_line(size = 0.5) +
   facet_wrap(~dataset, ncol = 3)
 
-ggsave("graphs/raw_data.pdf")
+ggsave("graphs/raw_data.pdf", width = 7, height = 5)
 
 # fit the data ------------------------------------------------------------
 
 x <- filter(spectra, wavelength == 350) %>% 
-  select(absorbance)
+  select(absorption)
 
 x <- as.vector(t(x))
 
@@ -24,14 +25,17 @@ wl <- seq(250, 450, by = 1)
 
 for(i in wl){
   y <- filter(spectra, wavelength == i) %>% 
-    select(absorbance)
+    select(absorption)
   
   y <- as.vector(t(y))
   
   fits[[i - 249]] <- lm(x ~ y)
+  #plot(x,y)
 }
 
-
+#---------------------------------------------------------------------
+# Extract intercept, slope and R2.
+#---------------------------------------------------------------------
 intercept <- unlist(lapply(fits, function(x){coef(x)[1]}))
 intercept_std <- unlist(lapply(fits, function(x){summary(x)$coefficients[1, 2]}))
 
@@ -39,7 +43,6 @@ slope <- unlist(lapply(fits, function(x){coef(x)[2]}))
 slope_std <- unlist(lapply(fits, function(x){summary(x)$coefficients[2, 2]}))
 
 r2 <- unlist(lapply(fits, function(x){summary(x)$r.squared}))
-
 
 # Graph on effect of wl ---------------------------------------------------
 
@@ -50,7 +53,7 @@ df <- data.frame(wl = wl,
                  slope_std = slope_std,
                  intercept_std = intercept_std)
 
-saveRDS(df, "data/clean/fit_cdom.rds")
+saveRDS(df, "dataset/clean/fit_cdom.rds")
 
 p1 <- ggplot(df, aes(x = wl, y = r2)) +
   geom_line() +
@@ -72,13 +75,9 @@ grid.arrange(p1, p2, p3)
 dev.off()
 
 
-p3 + ylim(min(x), max(x)) # A relative perspective
-
-
-
 # histogram ---------------------------------------------------------------
 df <- data.frame(x = x)
 ggplot(df, aes(x = x)) +
-  geom_histogram(binwidth = 0.01) +
-  geom_vline(x = 0.010, col = "red")
-
+  geom_histogram(binwidth = 10) +
+  geom_vline(x = mean(intercept), col = "red") +
+  xlab("a350")
