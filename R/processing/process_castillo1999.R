3dbfb08c1b445f77d5093c9c73d7ca85c3523bd0
Philippe Massicotte <pmassicotte@hotmail.com>
2015-11-11 10:43:30 +0100

    added data from castillo 1999

---
+++ R/processing/process_castillo1999.R
@@ -0,0 +1,16 @@
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# FILE:         process_castillo1999.R
+#
+# AUTHOR:       Philippe Massicotte
+#
+# DESCRIPTION:  Process raw data from Castillo et al. 1999
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+rm(list = ls())
+
+castillo1999 <- read_csv("dataset/raw/castillo1999/data.csv") %>% 
+
+  arrange(sample_id) 
+
+saveRDS(castillo1999, "dataset/clean/castillo1999.rds")
+
