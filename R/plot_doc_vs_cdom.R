304c3ee42d9355a95c2c6d09440147266fcdd371
Philippe Massicotte <pmassicotte@hotmail.com>
2016-01-28 15:32:49 +0100

    new script to plot data

--- R/plot_doc_vs_cdom.R
+++ R/plot_doc_vs_cdom.R
@@ -9,4 +9,26 @@
 
 rm(list = ls())
 
-dana12 <- readRDS("dataset/clean/stedmon/dana12.rds")
+cdom_doc <- readRDS("dataset/clean/complete_dataset.rds")
+
+cdom_doc <- filter(cdom_doc, wavelength == 350) %>% 
+  select(study_id, wavelength, absorption, doc)
+
+ggplot(cdom_doc, aes(x = doc, y = absorption)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~study_id, scales = "free") +
+  ylab(expression(a[CDOM(350)]))
+
+ggsave("graphs/acdom350_vs_doc.pdf", width = 10, height = 8)
+
+
+ggplot(cdom_doc, aes(x = doc, y = absorption, group = study_id)) +
+  geom_point(aes(color = study_id), alpha = 0.5) + 
+  geom_smooth(method = "lm", aes(color = study_id)) +
+  scale_x_log10() +
+  scale_y_log10() +
+  annotation_logticks() +
+  ylab(expression(a[CDOM(350)]))
+
+ggsave("graphs/acdom350_vs_doc2.pdf", width = 10, height = 8)
