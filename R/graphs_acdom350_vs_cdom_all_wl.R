c681506206b1089b59840007410e7f405a97681b
Philippe Massicotte <pmassicotte@hotmail.com>
2016-02-10 12:48:24 +0100

    graphic work

--- R/graphs_acdom350_vs_cdom_all_wl.R
+++ R/graphs_acdom350_vs_cdom_all_wl.R
@@ -8,32 +8,58 @@
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 rm(list = ls())
 
-tmp <- readRDS("dataset/clean/complete_dataset.rds") %>% 
-  filter(wavelength == 350) %>% 
-  select(absorption)
+get_data <- function(wl) {
+  
+  tmp <- readRDS("dataset/clean/complete_dataset.rds") %>% 
+    filter(wavelength == wl) %>% 
+    select(absorption)
+  
+  cdom_doc <- readRDS("dataset/clean/complete_dataset.rds") %>% 
+    group_by(wavelength) %>% 
+    nest() %>% 
+    mutate(model = map(data, ~ lm(tmp$absorption ~ .$absorption))) %>% 
+    unnest(map(model, broom::glance)) %>% 
+    unnest(map(model, broom::tidy))
 
-cdom_doc <- readRDS("dataset/clean/complete_dataset.rds") %>% 
-  group_by(wavelength) %>% 
-  nest() %>% 
-  mutate(model = map(data, ~ lm(tmp$absorption ~ .$absorption))) %>% 
-  unnest(map(model, broom::glance)) %>% 
-  unnest(map(model, broom::tidy))
+  names(cdom_doc) <- make.unique(names(cdom_doc))
+    
+  cdom_doc$term <- ifelse(cdom_doc$term == "(Intercept)", "intercept", "slope")
+  
+  cdom_doc$type = wl
+  
+  cdom_doc <- select(cdom_doc, wavelength, r.squared, term, estimate, type) %>% 
+    spread(term, estimate)
+  
+  return(cdom_doc)
+}
 
-p1 <- ggplot(cdom_doc, aes(x = wavelength, y = r.squared)) +
+res254 <- get_data(wl = 254) 
+res350 <- get_data(wl = 350) 
+res440 <- get_data(wl = 440)
+
+res <- bind_rows(res254, res350, res440)
+
+
+p1 <- ggplot(res, aes(x = wavelength, y = r.squared, color = factor(type))) +
   geom_point(size = 0.5) +
-  ylab(expression(R^2))
+  ylab(expression(R^2)) +
+  labs(color = "Target wl") +
+  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
+  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
 
-p2 <- ggplot(cdom_doc[cdom_doc$term == ".$absorption", ], aes(x = wavelength, y = estimate)) +
+p2 <- ggplot(res, aes(x = wavelength, y = slope, color = factor(type))) +
   geom_point(size = 0.5) +
-  geom_line(aes(y = estimate + std.error), col = "red", size = 0.1) +
-  geom_line(aes(y = estimate - std.error), col = "red", size = 0.1) +
-  ylab("Slope (m-1)")
+  ylab("slope") +
+  labs(color = "Target wl") +
+  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
+  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
 
-p3 <- ggplot(cdom_doc[cdom_doc$term == "(Intercept)", ], aes(x = wavelength, y = estimate)) +
+p3 <- ggplot(res, aes(x = wavelength, y = intercept, color = factor(type))) +
   geom_point(size = 0.5) +
-  geom_line(aes(y = estimate + std.error), col = "red", size = 0.1) +
-  geom_line(aes(y = estimate - std.error), col = "red", size = 0.1) +
-  ylab("Intercept (m-1)") 
+  ylab("intercept") +
+  labs(color = "Target wl") +
+  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
+  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
 
 ggsave("graphs/acdom350_vs_cdom_all_wl.pdf", 
        gridExtra::grid.arrange(p1, p2, p3, ncol = 1), 
