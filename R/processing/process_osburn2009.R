3145c557d522ba2c4b2c14bf330eab1a548c8ae1
Philippe Massicotte <pmassicotte@hotmail.com>
2016-02-22 11:09:56 +0100

    new data from osburn 2009

---
+++ R/processing/process_osburn2009.R
@@ -0,0 +1,21 @@
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# FILE:         process_osburn2009.R
+#
+# AUTHOR:       Philippe Massicotte
+#
+# DESCRIPTION:  Process raw data from Osburn et al. 2009
+# 
+# Osburn, C. L., Retamal, L., and Vincent, W. F. (2009). 
+# Photoreactivity of chromophoric dissolved organic matter transported by 
+# the Mackenzie River to the Beaufort Sea. Mar. Chem. 115, 10–20. 
+# doi:10.1016/j.marchem.2009.05.003.
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+rm(list = ls())
+
+osburn2009 <- read_csv("dataset/raw/literature/osburn2009/data_osburn2009.csv")
+
+saveRDS(osburn2009, "dataset/clean/literature/osburn2009.rds")
+
+ggplot(osburn2009, aes(x = doc, y = acdom)) +
+  geom_point()
