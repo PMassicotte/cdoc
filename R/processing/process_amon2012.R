cd73353e6b143903138b539561965199ec2a96bf
Philippe Massicotte <pmassicotte@hotmail.com>
2016-02-29 13:10:07 +0100

    using raw file from amon2012

--- R/processing/process_amon2012.R
+++ R/processing/process_amon2012.R
@@ -3,14 +3,29 @@
 #
 # AUTHOR:       Philippe Massicotte
 #
-# DESCRIPTION:  Process raw data from Amon et al. 2012
+# DESCRIPTION:  Process raw data from :
+# 
+# http://doi.pangaea.de/10.1594/PANGAEA.789137?format=html#download
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 rm(list = ls())
 
-amon2012 <- read_csv("dataset/raw/literature/amon2012/data.csv") %>% 
-  mutate(date = as.character(date)) %>% 
-  mutate(date = as.Date(date, format = "%d-%m-%y")) %>%
+amon2012 <- read_delim("dataset/raw/literature/amon2012/ARK-XXII_2_fluorescence_DOC.tab", 
+                       delim = "\t",
+                       skip = 164) %>% 
+  select(sample_id = Event,
+         date = `Date/Time`,
+         latitude = Latitude,
+         longitude = Longitude,
+         elevation = `Elevation [m]`,
+         depth = `Depth water [m]`,
+         doc = `DOC [mg/l]`,
+         acdom = `ac350 [1/m]`,
+         suva = `SUVA norm DOC [m**2/g]`) %>% 
+  mutate(doc = doc / 12 * 1000,
+         wavelength = 350,
+         study_id = "amon2012") %>% 
+  filter(!is.na(acdom) & !is.na(doc)) %>%
   mutate(unique_id = paste("amon2012",
                            as.numeric(interaction(study_id, 
                                                   sample_id, 
