bc2b1d508a2f7e0c02b0815834005e792afc5eae
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-11 11:22:18 +0100

    more test

--- R/tests/testthat/test_minimum_variables.R
+++ R/tests/testthat/test_minimum_variables.R
@@ -12,12 +12,12 @@ data <- lapply(files, readRDS)
 # 
 # lapply(data, f)
 
+minimal_variables <- c("acdom", "doc", "wavelength", "study_id", "sample_id", 
+                       "longitude", "latitude")
+
 context("Test common variables")
 
 test_that("Has all minimal set of variables", {
-  
-  minimal_variables <- c("acdom", "doc", "wavelength", "study_id", "sample_id", 
-                         "longitude", "latitude")
   
   expect_true(all(Reduce(intersect, lapply(data, names)) %in% minimal_variables))
   
@@ -27,11 +27,14 @@ test_that("Has appropriate numerical values", {
   
   expect_true(all(unlist(lapply(data, function(x){class(x$doc)})) %in% c("numeric", "integer")))
   
-  # All DOC value should be greater than 0
-  expect_true(all(unlist(lapply(data, function(x){x$doc})) > 0))
+  # All DOC and cdom values should be greater than 0
+  expect_true(all(unlist(lapply(data, function(x){x[, c("doc", "acdom")]})) > 0))
   
-  # All cdom value should be greater than 0
-  expect_true(all(unlist(lapply(data, function(x){x$acdom})) > 0))
 })
 
+test_that("Has no NA", {
+  
+  # No NA in all minimal variables
+  expect_false(any(unlist(lapply(data, function(x){is.na(x[, minimal_variables])}))))
 
+})
