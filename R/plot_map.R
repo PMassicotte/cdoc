1e1a708461e5ab25c2efba731960cb3c41fe8174
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-14 13:41:36 +0100

    color now based on ecotype

--- R/plot_map.R
+++ R/plot_map.R
@@ -9,25 +9,21 @@ rm(list = ls())
 
 literature <- readRDS("dataset/clean/literature_datasets.rds") %>% 
   filter(!is.na(longitude)) %>% 
-  select(longitude, latitude, study_id, sample_id, salinity) %>% 
-  distinct()
+  select(longitude, latitude, study_id, sample_id, ecotype)
 
 doc_cdom <- readRDS("dataset/clean/cdom_dataset.rds") %>% 
   filter(!is.na(longitude)) %>% 
-  select(longitude, latitude, study_id, sample_id, salinity) %>% 
-  distinct()
+  select(longitude, latitude, study_id, sample_id, ecotype)
+
+df <- bind_rows(literature, doc_cdom) %>% 
+  distinct(longitude, latitude)
 
 world <- cshp(date = as.Date("2008-1-1"))
 world.points <- fortify(world, region = 'COWCODE')
 
 ggplot(world.points, aes(long, lat, group = group)) + 
   geom_polygon() +
-  geom_point(data = literature, aes(x = longitude, 
-                                    y = latitude, 
-                                    group = NULL,
-                                    color = study_id), 
-             alpha = 1, size = 0.25) +
-  geom_point(data = doc_cdom, aes(x = longitude, 
+  geom_point(data = df, aes(x = longitude, 
                                     y = latitude, 
                                     group = NULL,
                                     color = study_id), 
@@ -43,8 +39,6 @@ ggsave("graphs/map.pdf", width = 15, height = 10)
 # Write a kml file.
 #---------------------------------------------------------------------
 
-df <- bind_rows(literature, doc_cdom)
-
 df <- data.frame(df)
 coordinates(df) <- c("longitude", "latitude")
 proj4string(df) <- CRS("+proj=longlat +datum=WGS84")
@@ -52,7 +46,6 @@ proj4string(df) <- CRS("+proj=longlat +datum=WGS84")
 plotKML::kml(df, 
              file = "dataset/datasets.kml", 
              size = 1,
-             colour = df$study_id,
+             colour = df$ecotype,
              shape = "http://maps.google.com/mapfiles/kml/pal2/icon18.png",
-             points_names = paste(df$study_id, df$sample_id, sep = "_"))
-
+             points_names = paste(df$ecotype, df$study_id, df$sample_id, sep = "_"))
