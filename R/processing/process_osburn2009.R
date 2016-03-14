35ec28cbaa9aa317977a22c09b73a104e9cc2567
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-14 12:32:42 +0100

    wrong id in castillo

--- R/processing/process_osburn2009.R
+++ R/processing/process_osburn2009.R
@@ -16,8 +16,8 @@ rm(list = ls())
 osburn2009 <- read_csv("dataset/raw/literature/osburn2009/data_osburn2009.csv") %>% 
   mutate(longitude = -longitude) %>% 
   filter(!is.na(doc) & !is.na(acdom)) %>% 
-  mutate(study_id = "castillo1999") %>% 
-  mutate(sample_id = paste("castillo1999", 1:nrow(.), sep = "_")) %>% 
+  mutate(study_id = "osburn2009") %>% 
+  mutate(sample_id = paste("osburn2009", 1:nrow(.), sep = "_")) %>% 
   mutate(ecotype = ifelse(salinity <= 0.1, "river", ifelse(salinity > 0.1 & salinity <= 25, "coastal", "ocean")))
 
 saveRDS(osburn2009, "dataset/clean/literature/osburn2009.rds")
