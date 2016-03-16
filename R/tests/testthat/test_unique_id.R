f464d59ae4947a018cec7a11c5a100565e5e9971
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-16 11:33:41 +0100

    test for unique_id

---
+++ R/tests/testthat/test_unique_id.R
@@ -0,0 +1,13 @@
+context("Test that unique_id is really unique")
+
+test_that("No duplicate in unique_id", {
+  
+  cdom_doc <- readRDS("dataset/clean/cdom_dataset.rds")
+  
+  res <- group_by(cdom_doc, wavelength, unique_id) %>% 
+    summarise(n = n()) %>% 
+    filter(n > 1)
+  
+  expect_true(nrow(res) == 0)
+  
+})
