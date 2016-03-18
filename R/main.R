9a32337738e7f0a4478829048becc8361eed88ca
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-18 09:59:57 +0100

    added code sections

--- R/main.R
+++ R/main.R
@@ -1,3 +1,5 @@
+# setup -------------------------------------------------------------------
+
 library(cshapes)
 library(gpclib)
 library(maptools)
@@ -25,6 +27,9 @@ graphics.off()
 ## Set default ggplot2 font size and font family
 loadfonts(quiet = TRUE)
 theme_set(theme_bw(base_size = 12, base_family = "Open Sans"))
+
+
+# processing --------------------------------------------------------------
 
 # ---------------------------------------------------------------------
 # Read and process 'raw' CDOM and DOC datasets.  The
