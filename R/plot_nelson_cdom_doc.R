d733dfb660e51f600528fc426f4f1f2381e77a23
Philippe Massicotte <pmassicotte@hotmail.com>
2016-03-23 11:05:08 +0100

    script to explore nelson data

---
+++ R/plot_nelson_cdom_doc.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+spc <- readRDS("dataset/clean/cdom_dataset.rds") %>% 
+  filter(study_id == "nelson")
+
+
+
+p1 <- filter(spc, wavelength == 325) %>% 
+ggplot(aes(x = doc, y = absorption)) +
+  geom_point(aes(color = depth), size = 1) +
+  geom_smooth(method = "lm") +
+  ylab("a325") +
+  ylim(0, 0.25) + 
+  scale_colour_gradientn(colours = rainbow(7))
+
+
+p2 <- filter(spc, wavelength == 275) %>% 
+  ggplot(aes(x = doc, y = absorption)) +
+  geom_point(aes(color = depth), size = 1) +
+  geom_smooth(method = "lm") +
+  ylab("a275") + 
+  scale_colour_gradientn(colours = rainbow(7))
+
+p <- grid.arrange(p1, p2)
+
+ggsave("graphs/nelson_doc_cdom.pdf", p, height = 8)
+
+metrics <- readRDS("dataset/clean/cdom_metrics.rds") %>% 
+  left_join(., spc) %>% 
+  filter(study_id == "nelson") %>% 
+  select(unique_id:doc, -river, -date, -suva254, depth, -suva440) %>% 
+  distinct()
+
+
+tmp <- gather(metrics, variable, value, suva350:sr)
+
+p3 <- ggplot(tmp, aes(x = doc, y = value)) +
+  geom_point(aes(color = depth), size = 0.1) +
+  geom_smooth(method = "lm") +
+  facet_wrap(~variable, scales = "free") +
+  scale_colour_gradientn(colours = rainbow(7))
+
+p3
+
+ggsave("graphs/nelson_doc_metrics.pdf", p3, width = 10, height = 5)
