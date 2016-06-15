55643fe365ac3964e16f23b91fa80b843a148d73
Philippe Massicotte <pmassicotte@hotmail.com>
2016-06-15 12:36:33 +0200

    new fig. 2

--- R/fig2.R
+++ R/fig2.R
@@ -1,15 +1,19 @@
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-# FILE:         graphs_acdom_vs_cdom_all_wl.R
-#
 # AUTHOR:       Philippe Massicotte
 #
 # DESCRIPTION:  Explore the relation between aCDOM350 and aCDOM at various
 #               wavelengths.
+#               
+#               This script produces figure 2 for the manuscript.
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 rm(list = ls())
 
 cdom_doc <- read_feather("dataset/clean/cdom_dataset.feather") %>%
   filter(study_id != "nelson") %>% # Nelson is missing wl < 275
+  filter(study_id != "greenland_lakes") %>%  # These had lamp problem at 360 nm
+  filter(study_id != "horsen") %>% 
+  filter(wavelength <= 500) %>% 
   select(unique_id, wavelength, absorption) %>%
   spread(wavelength, absorption) %>% 
   select(-unique_id)
@@ -35,42 +39,47 @@ get_data <- function(wl, cdom_doc) {
   return(df)
 }
 
-res254 <- get_data(wl = 254, cdom_doc)
+# res254 <- get_data(wl = 254, cdom_doc)
 res350 <- get_data(wl = 350, cdom_doc) 
-res440 <- get_data(wl = 440, cdom_doc)
+# res440 <- get_data(wl = 440, cdom_doc)
 
-res <- bind_rows(res254, res350, res440)
+# res <- bind_rows(res254, res350, res440)
 
-p1 <- ggplot(res, aes(x = wavelength, y = r.squared, color = factor(type))) +
+
+# Plot --------------------------------------------------------------------
+
+p1 <- ggplot(res350, aes(x = wavelength, y = r.squared)) +
   geom_point(size = 0.5) +
   ylab(expression(R^2)) +
-  labs(color = "Target wl") +
-  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
-  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
+  theme(axis.ticks = element_blank()) +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.text.x = element_blank()) +
+  annotate("text", Inf, Inf, label = "A",
+           vjust = 1.5, hjust = 1.5, size = 5, fontface = "bold")
 
-p2 <- ggplot(res, aes(x = wavelength, y = slope, color = factor(type))) +
+p2 <- ggplot(res350, aes(x = wavelength, y = slope)) +
   geom_point(size = 0.5) +
-  ylab("slope") +
-  labs(color = "Target wl") +
-  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
-  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
+  ylab("Slope") +
+  theme(axis.ticks = element_blank()) +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.text.x = element_blank()) +
+  annotate("text", Inf, Inf, label = "B",
+           vjust = 1.5, hjust = 1.5, size = 5, fontface = "bold")
 
-p3 <- ggplot(res, aes(x = wavelength, y = intercept, color = factor(type))) +
+p3 <- ggplot(res350, aes(x = wavelength, y = intercept)) +
   geom_point(size = 0.5) +
-  ylab("intercept") +
-  labs(color = "Target wl") +
-  geom_vline(xintercept = c(254, 350, 440), lty = 2, size = 0.1, color = c("red", "green", "blue")) +
-  scale_x_continuous(breaks = seq(240, 600, length.out = 10))
+  ylab(bquote("Intercept "*(m^{-1}))) +
+  xlab("Wavelength (nm)") +
+  annotate("text", Inf, Inf, label = "C",
+           vjust = 1.5, hjust = 1.5, size = 5, fontface = "bold")
 
-p <- cowplot::plot_grid(p1, p2, p3, ncol = 1)
-cowplot::save_plot("graphs/acdom_vs_cdom_all_wl.pdf", 
+p <- cowplot::plot_grid(p1, p2, p3, ncol = 1, align = "v", rel_heights = c(1,1,1.2))
+cowplot::save_plot("graphs/fig2.pdf", 
                    p, 
-                   base_height = 10,
-                   base_width = 7)
+                   base_height = 5,
+                   base_width = 3)
 
 # Raster plot -------------------------------------------------------------
-
-rm(list = ls())
 
 f <- function(x, y) {
   
@@ -81,7 +90,8 @@ f <- function(x, y) {
 
 cdom_doc <- read_feather("dataset/clean/cdom_dataset.feather") %>%
   filter(study_id != "nelson") %>% # Nelson is missing wl < 275
-  select(unique_id, wavelength, absorption) %>%
+  filter(study_id != "greenland_lakes") %>%  # These had lamp problem at 360 nm
+  filter(study_id != "horsen") %>% 
   filter(wavelength <= 500) %>% 
   group_by(wavelength) %>% 
   nest()
@@ -101,12 +111,20 @@ jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F"
 st <- "This shows the R2 of the linear regression between acdom at wl 1 against acdom at wl 2"
 st <- paste0(strwrap(st, 70), sep = "", collapse = "\n")
 
+
+# Plot --------------------------------------------------------------------
+
 ggplot(res, aes(x = wavelength, wavelength2, fill = r2)) +
   geom_raster() +
   scale_fill_gradientn(colours = rev(jet.colors(255))) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
-  ggtitle("Prediction of acdom at different wavelength", subtitle = st) +
-  coord_equal()
+  # ggtitle("Prediction of acdom at different wavelength", subtitle = st) +
+  coord_equal() +
+  ylab("Wavelength (nm)") +
+  xlab("Wavelength (nm)") +
+  labs(fill = bquote(R^2)) +
+  guides(fill = guide_colorbar(barwidth = 0.5)) 
 
-ggsave("graphs/acdom_vs_cdom_all_wl.png")
+ggsave("graphs/fig2_raster.png", dpi = 300)
+
