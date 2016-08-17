f801b3c84370a3eddd9af26f051e63091f42101b
Philippe Massicotte <pmassicotte@hotmail.com>
2016-08-17 14:24:52 +0200

    now using 3 endmembers

--- R/fig5.R
+++ R/fig5.R
@@ -7,21 +7,35 @@
 
 rm(list = ls())
 
+endmember <- function(ecosystem) {
+  
+  hm <- list(
+    "lake" = "Freshwater", 
+    "river" = "Freshwater", 
+    "sewage" = "Freshwater", 
+    "pond" = "Freshwater", 
+    "wetland" = "Freshwater",
+    "estuary" = "Coastal",
+    "coastal" = "Coastal",
+    "ocean" = "Ocean"
+  )
+  
+  return(unlist(hm[ecosystem], use.names = FALSE))
+  
+}
+
 cdom_complete <- read_feather("dataset/clean/cdom_dataset.feather") %>% 
   filter(wavelength <= 500) %>%
   filter(study_id != "nelson") %>% # Nelson is missing wl < 275
   filter(study_id != "greenland_lakes") %>%  # These had lamp problem at 360 nm
   filter(study_id != "horsen") %>% 
   filter(ecosystem != "brines") %>% 
-  mutate(endmember = ifelse(
-    ecosystem %in% c("lake", "river", "sewage", "pond", "wetland"),
-    "Freshwater",
-    "Marine"
-  )) %>% 
+  mutate(endmember = endmember(ecosystem)) %>% 
   group_by(wavelength, endmember) %>% 
   nest() %>% 
   mutate(model = purrr::map(data, ~lm(.$doc ~ .$absorption, data = .))) %>% 
-  unnest(model %>% purrr::map(broom::glance))
+  unnest(model %>% purrr::map(broom::glance)) %>% 
+  mutate(endmember = factor(endmember, c("Freshwater", "Coastal", "Ocean")))
 
 p <- cdom_complete %>% 
   ggplot(aes(x = wavelength, y = r.squared)) +
