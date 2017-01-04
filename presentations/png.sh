40217c44b6de84139b01e6694ea6a537a079954e
Philippe Massicotte <pmassicotte@hotmail.com>
2017-01-04 14:00:20 -0500

    first version of the ULAVAL presentation

---
+++ presentations/png.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig1.pdf" "myfigs/fig1"
+
+pdftoppm -r 300 -png -singlefile "../graphs/appendix1.pdf" "myfigs/appendix1"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig2.pdf" "myfigs/fig2"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig3.pdf" "myfigs/fig3"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig4.pdf" "myfigs/fig4"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig5.pdf" "myfigs/fig5"
+
+pdftoppm -r 300 -png -singlefile "../graphs/appendix3.pdf" "myfigs/appendix3"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig6.pdf" "myfigs/fig6"
+
+pdftoppm -r 300 -png -singlefile "../graphs/appendix4.pdf" "myfigs/appendix4"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig7.pdf" "myfigs/fig7"
+
+pdftoppm -r 300 -png -singlefile "../graphs/fig8.pdf" "myfigs/fig8"
+
+pdftoppm -r 300 -png -singlefile "../graphs/appendix5.pdf" "myfigs/appendix5"
