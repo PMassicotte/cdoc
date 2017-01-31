9ae7bc08317e95ae018ccd89485c2a0199ef7101
Philippe Massicotte <pmassicotte@hotmail.com>
2017-01-31 10:43:21 -0500

    removing supplementary 4 figure

--- R/appendix.R
+++ R/appendix.R
@@ -84,120 +84,6 @@ embed_fonts("graphs/appendix1.pdf")
 
 # Appendix 4 --------------------------------------------------------------
 
-# ***************************************************************************
-# Map showing the buffer area identified by the segmentation analysis around
-# the continents.
-# ***************************************************************************
-
-# http://rpsychologist.com/working-with-shapefiles-projections-and-world-maps-in-ggplot
-
-rm(list = ls())
-graphics.off()
-
-wmap <- readOGR("dataset/shapefiles/ne_110m_land/", "ne_110m_land")
-
-
-wmap <- spTransform(wmap, CRS = CRS("+proj=robin"))
-res <- gBuffer(wmap, width = 359.31 * 1000)
-
-wmap <- fortify(wmap)
-res <- fortify(res)
-
-grat <- readOGR("dataset/shapefiles/ne_110m_graticules_all/", 
-                layer = "ne_110m_graticules_15") 
-grat_df <- fortify(grat)
-
-bbox <- readOGR("dataset/shapefiles/ne_110m_graticules_all/", layer = "ne_110m_wgs84_bounding_box") 
-bbox_df <- fortify(bbox)
-grat_robin <- spTransform(grat, CRS("+proj=robin"))  # reproject graticule
-grat_df_robin <- fortify(grat_robin)
-bbox_robin <- spTransform(bbox, CRS("+proj=robin"))  # reproject bounding box
-bbox_robin_df <- fortify(bbox_robin)
-
-countries <- readOGR("dataset/shapefiles/ne_110m_admin_0_countries/",
-                     layer = "ne_110m_admin_0_countries") 
-countries_robin <- spTransform(countries, CRS("+init=esri:54030"))
-countries_robin_df <- fortify(countries_robin)
-
-coord <- data_frame(
-  longitude = seq(-180, 180, length.out = 5),
-  latitude = seq(-90, 90, length.out = 5)
-)
-
-coordinates(coord) <- c("longitude", "latitude")
-proj4string(coord) <- CRS("+proj=longlat +datum=WGS84")
-coord <- spTransform(coord, CRS = CRS("+proj=robin"))
-coord <- fortify(coord)
-
-latitude <- data_frame(
-  degree = seq(-90, 90, length.out = 5),
-  robin = coord$lat)
-
-coord <- data_frame(
-  longitude = c(-180, -60, 0, 60, 180),
-  latitude = seq(-90, 90, length.out = 5)
-)
-
-coordinates(coord) <- c("longitude", "latitude")
-proj4string(coord) <- CRS("+proj=longlat +datum=WGS84")
-coord <- spTransform(coord, CRS = CRS("+proj=robin"))
-coord <- fortify(coord)
-
-longitude <- data_frame(
-  degree = c(-180, -60, 0, 60, 180),
-  robin = coord$long)
-
-
-ggplot(bbox_robin_df, aes(long, lat, group = group)) +
-  geom_polygon(fill = "white") +
-  geom_polygon(data = res,
-               aes(x = long, y = lat, group = group),
-               fill = "#E41A1C") +
-  geom_polygon(data = wmap, aes(
-    x = long,
-    y = lat,
-    group = group,
-    fill = hole
-  )) +
-  # geom_polygon(data = countries_robin_df, aes(
-  #   long, 
-  #   lat, 
-  #   group = group, 
-  #   fill = hole
-  # )) + 
-  geom_path(
-    data = countries_robin_df,
-    aes(long, lat, group = group, fill = hole),
-    color = "gray50",
-    size = 0.05
-  ) + 
-  geom_path(
-    data = grat_df_robin,
-    aes(long, lat, group = group, fill = NULL),
-    lty = 2,
-    color = "grey50",
-    size = 0.1
-  ) +
-  coord_equal() +
-  scale_fill_manual(values = c("gray25", "white"), guide = "none") +
-  xlab("Longitude (degrees)") +
-  ylab("Latitude (degrees)") +
-  scale_x_continuous(breaks = longitude$robin,
-                     labels = longitude$degree) +
-  scale_y_continuous(breaks = latitude$robin,
-                     labels = latitude$degree) +
-  theme(panel.grid.minor = element_blank()) +
-  theme(panel.grid.major = element_blank()) +
-  theme(panel.background = element_blank()) +
-  theme(plot.background = element_rect(fill = "white"))
-
-ggsave("graphs/appendix4.pdf")
-system("pdfcrop graphs/appendix4.pdf graphs/appendix4.pdf")
-embed_fonts("graphs/appendix4.pdf")
-
-
-# Appendix 5 --------------------------------------------------------------
-
 df <- read_feather("dataset/clean/complete_data_350nm.feather") %>% 
   mutate(year = as.numeric(format(date, "%Y"))) %>% 
   mutate(month = format(date, "%B")) %>% 
@@ -247,8 +133,8 @@ pB <- res %>%
 p <- cowplot::plot_grid(pB, pA, ncol = 1, align = "v", rel_heights = c(1, 1.15))
 p
 
-save_plot("graphs/appendix5.pdf", p, base_width = 6, base_height = 5)
-embed_fonts("graphs/appendix5.pdf")
+save_plot("graphs/appendix4.pdf", p, base_width = 6, base_height = 5)
+embed_fonts("graphs/appendix4.pdf")
 
 df %>%
   group_by(hemisphere) %>% 
