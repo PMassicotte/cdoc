4ca5cabc82c6bc23dedab188d9200f42cc9fbfb6
Philippe Massicotte <pmassicotte@hotmail.com>
2017-06-16 14:27:19 -0400

    Modified Fig. 7 to include spectral slope curve by ecosystem (STOTEN review).

--- R/fig7.R
+++ R/fig7.R
@@ -7,15 +7,21 @@
 
 rm(list = ls())
 
+
 cdom_complete <- read_feather("dataset/clean/cdom_dataset.feather") %>% 
   filter(wavelength <= 500) %>%
   filter(study_id != "nelson") %>% # Nelson is missing wl < 275
   filter(study_id != "greenland_lakes") %>%  # These had lamp problem at 360 nm
-  filter(study_id != "horsen") %>% 
+  filter(study_id != "horsen")
+
+## Normalize to maximum value
+cdom_complete <- cdom_complete %>% 
   group_by(unique_id) %>% 
   mutate(absorption = absorption / max(absorption)) %>% 
-  ungroup() %>% 
-  mutate(endmember = ifelse(ecosystem %in% c("lake", "river"), "Freshwater", "Marine")) %>% 
+  ungroup()
+
+cdom_complete_by_endmember <- cdom_complete %>% 
+  mutate(endmember = ifelse(ecosystem %in% c("lake", "river"), "Freshwater", "Marine")) %>%
   group_by(wavelength, endmember) %>%
   summarise(absorption = mean(absorption)) %>% 
   ungroup() %>% 
@@ -24,8 +30,15 @@ cdom_complete <- read_feather("dataset/clean/cdom_dataset.feather") %>%
   mutate(model = purrr::map(data, ~cdom_spectral_curve(.$wavelength, .$absorption, r2threshold = 0.8))) %>% 
   unnest(model)
 
-
-# Plot --------------------------------------------------------------------
+cdom_complete_by_ecosystem <- cdom_complete %>% 
+  group_by(wavelength, ecosystem) %>%
+  summarise(absorption = mean(absorption)) %>% 
+  ungroup() %>% 
+  mutate(endmember = ifelse(ecosystem %in% c("lake", "river"), "Freshwater", "Marine")) %>% 
+  group_by(ecosystem, endmember) %>% 
+  nest() %>%  
+  mutate(model = purrr::map(data, ~cdom_spectral_curve(.$wavelength, .$absorption, r2threshold = 0.8))) %>% 
+  unnest(model)
 
 df <- tibble(
   ymin = c(rep(0.0082, 6)),
@@ -35,10 +48,20 @@ df <- tibble(
   label = c("I", "II", "III", "IV", "V", "VI")
 )
 
-p <- cdom_complete %>%
+p <- cdom_complete_by_endmember %>% 
   ggplot(aes(x = wl, y = s)) +
-  # geom_vline(data = df, aes(xintercept = xmax), lty = 2, size = 0.25) +
-  # geom_vline(data = df, aes(xintercept = xmin), lty = 2, size = 0.25) +
+  geom_line(aes(color = r2)) +
+  facet_wrap(~endmember) +
+  scale_color_gradientn(
+    colours = viridis(255, end = 0.75),
+    guide = guide_colorbar(
+      direction = "vertical", 
+      tick = TRUE,
+      barwidth = 0.75, 
+      barheight = 2,
+      label.theme = element_text(size = 8, angle = 0))
+  ) +
+  geom_line(data = cdom_complete_by_ecosystem, aes(x = wl, y = s, linetype = ecosystem), color = "gray75") +
   geom_segment(
     data = df,
     aes(
@@ -53,21 +76,6 @@ p <- cdom_complete %>%
       length = unit(0.1, "cm")
     ), size = 0.25
   ) +
-  # geom_rect(
-  #   data = myrect1,
-  #   aes(
-  #     ymin = ymin,
-  #     ymax = ymax,
-  #     xmax = xmax,
-  #     xmin = xmin
-  #   ),
-  #   fill = "gray",
-  #   alpha = 0.35,
-  #   inherit.aes = F,
-  #   color = "black", 
-  #   linetype = "dashed",
-  #   size = 0.1
-  # ) +
   geom_text(
     data = df,
     aes(
@@ -77,32 +85,22 @@ p <- cdom_complete %>%
     ),
     inherit.aes = F,
     fontface = 2,
-    vjust = -2
+    vjust = -3
   ) +
   geom_text(data = df, aes(
     x = xmin + (xmax - xmin) / 2,
     y = -Inf,
     label = paste(xmin, xmax, sep = " - ")
-  ), inherit.aes = F, size = 2, vjust = -0.5, fontface = 3) +
-  
-  geom_line(aes(color = r2)) +
-  facet_wrap(~endmember) +
-  scale_color_gradientn(
-    colours = viridis(255, end = 0.75),
-    guide = guide_colorbar(
-      direction = "vertical", 
-      tick = TRUE,
-      barwidth = 0.75, 
-      barheight = 2,
-      label.theme = element_text(size = 8, angle = 0))
-  ) +
+  ), inherit.aes = F, size = 2, vjust = -1.5, fontface = 3) +
   xlab("Wavelength (nm)") +
   ylab(bquote(S[lambda]~(nm^{-1}))) +
-  labs(color = bquote(italic(R^2))) +
-  theme(legend.justification = c(1, 1), legend.position = c(0.98, 0.98)) +
+  labs(color = bquote(italic(R^2)),
+       linetype = "Ecosystems") +
+  theme(legend.justification = c(1, 1), legend.position = c(0.48, 0.98)) +
   scale_x_continuous(expand = c(0.08, 0)) +
-  # theme(legend.background = element_rect(fill = alpha(0.4))) +
+  theme(legend.box = "horizontal") +
   ylim(0.0075, 0.028)
 
-ggsave("graphs/fig7.pdf", p, height = 3, width = 7)
+ggsave("graphs/fig7.pdf", height = 4, width = 8)
 embed_fonts("graphs/fig7.pdf")
+
